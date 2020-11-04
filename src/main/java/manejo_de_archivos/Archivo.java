fe88dae085070de81ad1788a9656a8e85da04af5
Francisco A <framoron8@gmail.com>
2020-11-04 11:54:33 -0300

    1!

---
+++ src/main/java/manejo_de_archivos/Archivo.java
@@ -0,0 +1,14 @@
+package manejo_de_archivos;
+
+public class Archivo{
+    public static String ruta;
+    public void Archivo(){}
+    
+    public static void main(String[] args) {
+        ruta="dsjfjfdlkjfdslk";
+        System.out.println(ruta);
+    }
+    
+}
+
+
